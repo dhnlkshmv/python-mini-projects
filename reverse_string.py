83b6d2b16d91d11c4c651f3117cc30fcdec23464
Dhanalakshmi Venkatesan <dvenkat17@gmail.com>
2020-09-25 14:57:12 +0530

    create reverse_string

---
+++ reverse_string.py
@@ -0,0 +1,11 @@
+# Created by xvidviii
+
+# Reverse a given string
+
+# Example 1
+str = "The quick brown fox jumps over the lazy dog"
+print("The reverse string: ", str[::-1]) # reserves the string
+
+# Example 2
+str_1 = "god yzal eht revo spmuj xof nworb kciuq ehT"
+print("The reverse string: ", str_1[::-1])
