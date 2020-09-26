e3900b4bb1b2907c9681d649fa29a9e92a53ce5e
Dhanalakshmi Venkatesan <dvenkat17@gmail.com>
2020-09-26 20:56:19 +0530

    added print function

--- reverse_string.py
+++ reverse_string.py
@@ -4,8 +4,10 @@
 
 # Example 1
 str = "The quick brown fox jumps over the lazy dog"
-print("The reverse string: ", str[::-1]) # reserves the string
-
+print("Original string: {}".format(str))
+print("Reverse string: ", str[::-1]) # reserves the string
+print() # added a new line
 # Example 2
 str_1 = "god yzal eht revo spmuj xof nworb kciuq ehT"
-print("The reverse string: ", str_1[::-1])
+print("Original string: {}".format(str_1))
+print("Reverse string: ", str_1[::-1])
